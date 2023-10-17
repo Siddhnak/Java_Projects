d459bf0d69e29f87362406b1d57ed3b854d94909
Siddhnak <siddhnak@gmail.com>
2023-10-17 09:49:31 +0530

    Completed with last delete

--- crud-rest-app/src/main/java/net/sidnaik/springboot/rest/api/service/impl/UserServiceImpl.java
+++ crud-rest-app/src/main/java/net/sidnaik/springboot/rest/api/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import net.sidnaik.springboot.rest.api.repository.UserRepository;
 import net.sidnaik.springboot.rest.api.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,5 +27,36 @@ get() method returns a User*/
         Optional<User> optionalUser = userRepository.findById(userId);
         return optionalUser.get();  //it was null here
 //        return null;
+    }
+    // L65 GetAllUsers rest API
+    @Override
+    public List<User> getAlluser() /* will get all users from the DB*/ {
+        return userRepository.findAll();
+    }
+
+    @Override
+
+    public User updateUser(User user) {
+        /*Well, let's first get the existing User
+         object from the database and then we'll update that User object.*/
+        User existingUser = userRepository.findById(user.getId()).get();
+        /* let's call User.getId() and this findById() method returns
+                 optional of type User so.. So let's call a get() method.*/
+
+/*
+        Next, what we'll do, we'll update all the information from
+        this User object into this existing User.*/
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        userRepository.save(existingUser);
+        User updatedUser= userRepository.save(existingUser);
+        return updatedUser;
+
+    }
+
+
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
     }
 }
