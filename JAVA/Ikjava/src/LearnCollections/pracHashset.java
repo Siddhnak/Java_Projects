b01454cce1fd81a32f77d3776d40ffe77ad5d1ed
Siddhant Naik <Siddhant.Naik@lntinfotech.com>
2023-10-19 16:58:02 +0530

    Practiced hashset

--- JAVA/Ikjava/src/LearnCollections/pracHashset.java
+++ JAVA/Ikjava/src/LearnCollections/pracHashset.java
@@ -1,5 +1,6 @@
 package LearnCollections;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -14,11 +15,50 @@ public class pracHashset {
         set.add("Four");
         set.add("Five");
         set.add("Five"); //check for duplicate
-        Iterator<String> i=set.iterator();
-        while(i.hasNext())
-        {
-            System.out.println(i.next());
+        set.add("Bablya"); //check for duplicate
+        set.add("babli"); //check for duplicate
+        //Traversing elements
+//
+//        Iterator<String> i=set.iterator();
+//        while(i.hasNext())
+//        {
+//            System.out.println(i.next());
+//        }
+        set.remove("Five");
+        System.out.println("After invoking remove(object) method: "+set);
+
+        HashSet<String> set1=new HashSet();
+        set1.add("babu");
+        set1.add("raju");
+        set1.add("Shyam");
+        set.addAll(set1);
+        System.out.println("here is the updated set.."+set);
+
+        //Removing all the new elements we added into the set
+
+        set.removeAll(set1);
+        System.out.println("here's after removing the new set we added:"+set);
+
+        System.out.println("Pre removing Bablya"+set);
+        set.removeIf(str->str.contains("Bablya"));
+        System.out.println("After invoking removeIf() method: "+set);
+
+
+//   Java HashSet from another Collection
+        System.out.println("HashSet by passing arraylist's in the parameter");
+        ArrayList<String>arlist=new ArrayList<>();
+        arlist.add("Batman");
+        arlist.add("Flash");
+        arlist.add("Wwoman");
+
+        HashSet<String> set3= new HashSet<>(arlist);
+        set3.add("HUlk");
+        for (String s : set3) {
+            System.out.println(s);
+
         }
+// Leaving out the book example now..
+
     }
 
 }
